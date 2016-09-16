1dc1a1be28da794f7f9e97a2cf0e74954dbaf113
Joe Phearse <jphearse@gmail.com>
2016-09-16 15:25:11 -0500

    made artist and tites values not null

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,8 +2,8 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(255),
-    name VARCHAR(255),
+    artist VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     release_date INT,
     sales FLOAT,
     genre VARCHAR(255),
