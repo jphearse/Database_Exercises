be9b8b105f3f922d42c1fa7786031a15dabd0a5a
Joe Phearse <jphearse@gmail.com>
2016-09-19 10:20:41 -0500

    changed name_date to name_year

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    release_date INT,
+    release_year INT,
     sales FLOAT,
     genre VARCHAR(255),
     PRIMARY KEY (id)
