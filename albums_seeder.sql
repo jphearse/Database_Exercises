9270a880a06d0361903c6a6e89c1c5d262983137
Joe Phearse <jphearse@gmail.com>
2016-09-19 10:22:58 -0500

    changed select options

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -375,8 +375,8 @@ VALUES
 'Heavy metal, hard rock');
 
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
-SELECT genre FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT release_year FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name FROM albums WHERE release_year BETWEEN '1990' AND '1999';
 SELECT name FROM albums WHERE sales < '20000000';
-SELECT * FROM albums WHERE name = 'Nevermind';
-SELECT name  FROM albums WHERE release_year BETWEEN '1990' AND '1999';
- SELECT * FROM albums WHERE genre LIKE '%rock';
+ SELECT * FROM albums WHERE genre LIKE '%rock%';
