a59259af431deb7e29f3b9ab806c8345c67989d4
Joe Phearse <jphearse@gmail.com>
2016-09-19 14:22:15 -0500

    added select

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -1,5 +1,9 @@
 USE codeup_test_db;
 
 DELETE FROM albums WHERE release_year > 1991;
+
 DELETE FROM albums WHERE genre = 'disco';
+
 DELETE FROM albums WHERE artist = 'Meat Loaf';
+
+SELECT * FROM albums;
