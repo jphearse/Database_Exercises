af504ca99daa5a26d7fda8d5370abeead104273f
Joe Phearse <jphearse@gmail.com>
2016-09-20 10:17:12 -0500

    added sorting conditions

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,9 @@
 SELECT * FROM employees;
-SELECT * FROM employees WHERE gender = 'M' AND (first_name IN ('Irena', 'Vidya') OR first_name = 'Maya');
+SELECT * FROM employees WHERE gender = 'M' AND (first_name IN ('Irena', 'Vidya') OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name DESC;
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name  LIKE '%q%' AND last_name NOT LIKE '%qu%';
